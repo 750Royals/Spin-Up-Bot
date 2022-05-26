dfe491b7d751c15b2c6bb731061f485227e3dbc4
mayankd101 <69910481+mayankd101@users.noreply.github.com>
2022-05-26 17:36:04 -0400

    Add files via upload

---
+++ drive.cpp
@@ -0,0 +1,87 @@
+#include "main.h"
+
+
+
+double leftY = deadzone(controller.get_analog(ANALOG_LEFT_Y),10);
+double leftX = deadzone(controller.get_analog(ANALOG_LEFT_X),10);
+double rightY = deadzone(controller.get_analog(ANALOG_RIGHT_Y),10);
+double rightX = deadzone(controller.get_analog(ANALOG_RIGHT_X),10);
+
+
+void setDriveVoltage(double voltage)
+{
+  backRight.move_voltage(voltage);
+  frontRight.move_voltage(voltage);
+  backLeft.move_voltage(voltage);
+  frontLeft.move_voltage(voltage);
+}
+
+void resetDriveMotors()
+{
+  backRight.tare_position();
+  frontRight.tare_position();
+  backLeft.tare_position();
+  frontLeft.tare_position();
+}
+
+double convert(double inches)
+{
+  double diameter = 4.00;
+  double circumference = diameter*M_PI;
+  return (300*inches)/circumference;
+}
+
+double deadzone(double value, double deadzone)
+{
+  if(fabs(value)<deadzone)
+  {
+    return 0;
+  }
+  return value;
+}
+
+
+void PID::moveDistance(double distance)
+{
+  while(fabs(error)>0.0001)
+  {
+    target = convert(distance);
+    current = (backLeft.get_position()+backRight.get_position()+frontLeft.get_position()+frontRight.get_position())/4;
+    error = current - target;
+    derivative = error - prevError;
+    integral += error;
+    setDriveVoltage(kP*error+kI*integral+kD*derivative);
+    prevError = error;
+    pros::delay(20);
+  }
+  resetDriveMotors();
+}
+
+
+
+void setDrive(double kP, double kI, double kD)
+{
+  drive.kP = kP;
+  drive.kI = kI;
+  drive.kD = kD;
+}
+
+
+
+double curveControls(double value, double min, int exponent)
+{
+  if(value>0)
+  {
+    return ((1-(min/127))*pow((value/127),exponent)+(min/127))*127;
+  }
+  else if(exponent%2==0 && value<0)
+  {
+    return (((min/127)-1)*pow((value/127),exponent)-(min/127))*127;
+  }
+  else if(value<0)
+  {
+    return ((1-(min/127))*pow((value/127),exponent)-(min/127))*127;
+  }
+  return 0;
+}
+
---
+++ globals.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+pros::Motor backRight(1, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor frontRight(2, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor backLeft(3, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor frontLeft(4, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
