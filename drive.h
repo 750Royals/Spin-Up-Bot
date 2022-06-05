54a28b2f6955d094b7cdb1a62cc8079678020506
mayankd101 <69910481+mayankd101@users.noreply.github.com>
2022-06-05 19:20:01 -0400

    Update drive.h

--- drive.h
+++ drive.h
@@ -3,17 +3,13 @@
 extern PID linearDrive;
 extern PID rotationalDrive;
 
-extern double leftY;
-extern double leftX;
-extern double rightY;
-extern double rightX;
-double deadzone(double value, double deadzone);
+int deadzone(int value, int deadzone);
 void setDriveVoltage(double voltage);
 void resetDriveMotors();
 double convert(double inches);
 void setLinearDrive(double kP, double kI, double kD);
 void setAngularDrive(double kP, double kI, double kD);
-double curveControls(double value, double min, int exponent);
+double curveControls(int value, double min, int exponent);
 void setDriveControls();
 
 
