ce9e7904e0278e057939102354db42a05aee522f
mayankd101 <69910481+mayankd101@users.noreply.github.com>
2022-06-12 12:29:25 -0400

    Delete headers directory

--- headers/globals.h
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-extern pros::Controller controller;
-
-extern pros::Motor backRight;
-extern pros::Motor frontRight;
-extern pros::Motor backLeft;
-extern pros::Motor frontLeft;
-
