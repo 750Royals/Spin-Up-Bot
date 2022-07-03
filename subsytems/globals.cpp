8e26c82fa8cb4288d88dcce70c90ca40d70eeb73
dilan0509 <101671298+dilan0509@users.noreply.github.com>
2022-07-02 20:32:29 -0400

    Update globals.cpp

--- subsytems/globals.cpp
+++ subsytems/globals.cpp
@@ -6,3 +6,5 @@ pros::Motor backRight(1, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_C
 pros::Motor frontRight(2, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor backLeft(3, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor frontLeft(4, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor intake(5, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor flywheel(6, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
