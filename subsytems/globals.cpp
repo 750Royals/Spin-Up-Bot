60df1a48d7844eb9cf0ae2436bd1948bf1f2fd5d
mayankd101 <69910481+mayankd101@users.noreply.github.com>
2022-07-10 11:49:13 -0400

    Update globals.cpp

--- subsytems/globals.cpp
+++ subsytems/globals.cpp
@@ -2,9 +2,9 @@
 
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
-pros::Motor backRight(1, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor backRight(10, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor frontRight(2, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor backLeft(3, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor frontLeft(4, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor intake(5, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor intake(1, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor flywheel(6, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
