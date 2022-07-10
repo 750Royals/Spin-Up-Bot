2e4d82169e8ff8b72047c9833199a0a4007feb43
dilan0509 <101671298+dilan0509@users.noreply.github.com>
2022-07-10 11:56:02 -0400

    Create drive.cpp

--- subsytems/drive.cpp
+++ subsytems/drive.cpp
@@ -52,7 +52,7 @@ void setDriveControls()
     backRight.move(rightMove);
   
     //Constant Itake
-    intake.move_velocity(250);
+    intake.move_velocity(300);
   
     //Flywheel Code
     if(master.get_digital(DIGITAL_X))
