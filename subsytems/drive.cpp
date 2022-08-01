cdd8895097d521b2dac6eec06580123eb6f110a9
10021913 <74271045+10021913@users.noreply.github.com>
2022-08-01 13:53:58 +0530

    Update drive.cpp

--- subsytems/drive.cpp
+++ subsytems/drive.cpp
@@ -47,10 +47,10 @@ void setDriveControls()
     double turn = curveControls(rightX, 10, 3);
     int leftMove = power + turn;
     int rightMove = power - turn;
-    frontLeft.move(leftMove*0.6);
-    backLeft.move(leftMove*0.6);
-    frontRight.move(rightMove*0.6);
-    backRight.move(rightMove*0.6);
+    frontLeft.move(leftMove);
+    backLeft.move(leftMove);
+    frontRight.move(rightMove);
+    backRight.move(rightMove);
  
     //Constant Itake
     //Toggle Switch for Intake Speed
