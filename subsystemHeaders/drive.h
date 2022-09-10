feb58487f16e56768092f23265fffd8db7974c05
mayankd101 <69910481+mayankd101@users.noreply.github.com>
2022-09-10 16:35:05 -0400

    Update drive.h

--- subsystemHeaders/drive.h
+++ subsystemHeaders/drive.h
@@ -5,5 +5,5 @@ void setDriveVoltage(double voltage);
 void resetDriveMotors();
 double convert(double inches);
 double curveControls(int value, double min, int exponent);
-void setDriveControls();
-
+void setDriverControls();
+void indexerControl();
