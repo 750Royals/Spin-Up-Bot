e0c88a042784b956334a47e2ef957b65da31b7b7
anikat2 <69318995+anikat2@users.noreply.github.com>
2022-11-02 16:48:39 -0400

    Update globals.cpp

--- subsytems/globals.cpp
+++ subsytems/globals.cpp
@@ -8,5 +8,5 @@ pros::Motor backLeft(1, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_CO
 pros::Motor frontLeft(9, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor intake(7, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor flywheel(5, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor indexer(2, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor indexer(2, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor roller(3, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
