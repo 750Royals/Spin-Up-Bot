1512170f2123c15f7bdbb6521e6a3eaa9e22c06f
mayankd101 <69910481+mayankd101@users.noreply.github.com>
2022-12-10 23:51:19 -0500

    Update autonomous.cpp

--- subsytems/autonomous.cpp
+++ subsytems/autonomous.cpp
@@ -12,47 +12,43 @@ ChassisControllerBuilder()
 
 void right_auton()
 {
-  chassis->moveDistance(1.1_ft);//Move forward diagonally
-  pros::delay(3);
-  chassis->turnAngle(45_deg);//Turn left so the bot roller faces field roller
-  pros::delay(3);
-  chassis->moveDistance(-2.0_ft);//Move backward and spin roller
-  pros::delay(3);
-  chassis->moveDistance(5.1_ft);//Move forward and collect discs
-  pros::delay(3);
-  chassis->turnAngle(-90_deg);//Turn right and face the high goal
-  pros::delay(3);
-  flywheel.move_velocity(600); //Shoot discs into high goal
-  pros::delay(3);
+  chassis->moveDistance(1.5_ft);
+  chassis->turnAngle(-90_deg);
+  chassis->moveDistance(-1.5_ft);
+  chassis->turnAngle(90_deg);
+  chassis->moveDistance(-1.6_ft);
+  roller.move_velocity(-600);
+  pros::delay(800);
+  roller.move_velocity(0);
+  chassis->turnAngle(-90_deg);
+  flywheel.move_velocity(-400);
+  pros::delay(4000);
+  indexer.move_relative(45, 100);
+  pros::delay(1000);
+  indexer.move_relative(45, 100);
+  pros::delay(1000);
 }
 
 void left_auton()
 {
-  chassis->moveDistance(-0.1_ft);
-  pros::delay(20);
-  roller.move_velocity(200);
-  pros::delay(20);
-  chassis->moveDistance(1_ft);
+  chassis->moveDistance(-.80_ft);
+  roller.move_velocity(-600);
+  pros::delay(800);
+  roller.move_velocity(0);
+  pros::delay(100);
+  chassis->turnAngle(300_deg);
+  flywheel.move_velocity(-300);
+  pros::delay(4000);
+  indexer.move_relative(45, 100);
+  pros::delay(1000);
+  indexer.move_relative(45, 100);
+  pros::delay(1000);
+  indexer.move_relative(45, 100);
+  pros::delay(1000);
 }
 
 void skills_auton()
 {
-  /*
-  chassis->moveDistance(-.80_ft);//Slightly move forward
-  roller.move_velocity(-600); //Rotates initial field roller
-  pros::delay(800);
-  roller.move_velocity(0);
-  pros::delay(100);
-  chassis->turnAngle(-10_deg);
-  flywheel.move_velocity(-300);
-  pros::delay(2000);
-  indexer.move_relative(52, 100);
-  pros::delay(1000);
-  indexer.move_relative(52, 100);
-  pros::delay(1000);
-  /*
-
-
   /*chassis->moveDistance(4.75_ft);//Slightly move forward
   pros::delay(100);
   chassis->turnAngle(-180_deg);//Turn left
