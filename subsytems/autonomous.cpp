cc8f97820fe737050abded47e3642c8a383cd836
mayankd101 <69910481+mayankd101@users.noreply.github.com>
2022-12-17 20:15:22 -0500

    Update autonomous.cpp

--- subsytems/autonomous.cpp
+++ subsytems/autonomous.cpp
@@ -6,12 +6,19 @@ std::shared_ptr<ChassisController> chassis =
 ChassisControllerBuilder()
 .withMotors(leftSet, rightSet)
 .withDimensions(AbstractMotor::gearset::blue, {{4_in, 9.5_in}, okapi::imev5BlueTPR})
-.withMaxVelocity(300)
+.withMaxVelocity(500)
 .build();
+
+
 
 
 void right_auton()
 {
+  frontLeft.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  backLeft.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  frontRight.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  backRight.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+
   chassis->moveDistance(1.5_ft);
   chassis->turnAngle(-90_deg);
   chassis->moveDistance(-1.5_ft);
@@ -31,12 +38,62 @@ void right_auton()
 
 void left_auton()
 {
-  chassis->moveDistance(-.80_ft);
+  indexer.move_relative(170, 80);
+  pros::delay(50);
+  indexer.move_absolute(170, 80);
+  /*indexer.move_relative(170, 80);
+  indexer.move_relative(170, 80);
+
+  frontLeft.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  backLeft.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  frontRight.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  backRight.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+
+  flywheel.move_velocity(-399);
+  chassis->moveDistance(-0.2_ft);
   roller.move_velocity(-600);
-  pros::delay(800);
+  pros::delay(200);
   roller.move_velocity(0);
   pros::delay(100);
-  chassis->turnAngle(300_deg);
+  chassis->moveDistance(0.5_ft);
+  pros::delay(100);
+  chassis->turnAngle(50_deg);
+  pros::delay(50);
+  chassis->moveDistance(2_ft);
+  pros::delay(50);
+  chassis->turnAngle(-108_deg);
+  pros::delay(50);
+  chassis->moveDistance(0.6_ft);
+  pros::delay(50);
+  indexer.move_relative(170, 80);
+  pros::delay(3000);
+*/
+}
+
+void left_auton_backup()
+{
+
+  frontLeft.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  backLeft.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  frontRight.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  backRight.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+
+  chassis->moveDistance(-.50_ft);
+  roller.move_velocity(-600);
+  pros::delay(500);
+  roller.move_velocity(0);
+  pros::delay(100);
+  chassis->moveDistance(0.7_ft);
+  chassis->turnAngle(130_deg);
+  pros::delay(50);
+  flywheel.move_velocity(-300);
+  pros::delay(2000);
+  indexer.move_relative(170, 100);
+  pros::delay(1000);
+  indexer.move_relative(170, 100);
+  pros::delay(50);
+
+  /*chassis->turnAngle(300_deg);
   flywheel.move_velocity(-300);
   pros::delay(4000);
   indexer.move_relative(45, 100);
@@ -44,7 +101,7 @@ void left_auton()
   indexer.move_relative(45, 100);
   pros::delay(1000);
   indexer.move_relative(45, 100);
-  pros::delay(1000);
+  pros::delay(1000);/*
 }
 
 void skills_auton()
