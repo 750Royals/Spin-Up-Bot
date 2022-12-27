652d4aefb6b8d23a143a76e46652329c9bd66fb4
mayankd101 <69910481+mayankd101@users.noreply.github.com>
2022-12-27 16:32:18 -0500

    Update globals.h

--- subsystemHeaders/globals.h
+++ subsystemHeaders/globals.h
@@ -6,6 +6,8 @@ extern pros::Motor backRight;
 extern pros::Motor frontRight;
 extern pros::Motor backLeft;
 extern pros::Motor frontLeft;
+extern pros::Motor backLeftUp;
+extern pros::Motor backRightUp;
 extern pros::Motor intake;
 extern pros::Motor flywheel;
 extern pros::Motor indexer;
