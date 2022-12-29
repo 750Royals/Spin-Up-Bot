1f2be9b62b505540c9dcdf62a763725e04e07436
mayankd101 <69910481+mayankd101@users.noreply.github.com>
2022-12-29 13:12:45 -0500

    Update pid.cpp

--- subsytems/pid.cpp
+++ subsytems/pid.cpp
@@ -1,14 +1,27 @@
 #include "main.h"
 
-void setDriveVoltage(double voltage)
+double kP = 0.1;
+double kI = 0.1;
+double kD = 0.1;
+
+void setDriveVoltage(int leftVoltage, int rightVoltage)
 {
-  backRight.move_voltage(voltage);
-  frontRight.move_voltage(voltage);
-  backLeft.move_voltage(voltage);
-  frontLeft.move_voltage(voltage);
+  backRight.move_voltage(rightVoltage);
+  frontRight.move_voltage(rightVoltage);
+  backLeft.move_voltage(leftVoltage);
+  frontLeft.move_voltage(leftVoltage);
 }
 
-void resetDriveMotors()
+int convert(double inches)
+{
+  double diameter = 4.00;
+  double circumference = diameter*M_PI;
+  double gearRatio = 1.0;
+  return (int)((300*inches*7)/(circumference*3));
+}
+
+
+void resetDriveEncoders()
 {
   backRight.tare_position();
   frontRight.tare_position();
@@ -17,27 +30,22 @@ void resetDriveMotors()
 }
 
 
-void PID::moveDistance(double distance)
+void moveDistance(double distance)
 {
   while(fabs(error)>0.0001)
   {
     target = convert(distance);
-    current = (backLeft.get_position()+backRight.get_position()+frontLeft.get_position()+frontRight.get_position())/4;
-    error = current - target;
+    int leftMotorValues = (backLeft.get_position() + frontLeft.get_position() + backLeftUp.get_position())/3;
+    int rightMotorValues = (backRight.get_position() + frontRight.get_position() + backRightUp.get_position())/3;
+    current = (leftMotorValues + rightMotorValues)/2;
+    error = target - current;
     derivative = error - prevError;
     integral += error;
-    setDriveVoltage(kP*error+kI*integral+kD*derivative);
+    int leftVoltage = (int) (1000*(kP*error+kI*integral+kD*derivative));
+    int rightVoltage = (int) (1000*(kP*error+kI*integral+kD*derivative));
+    setDriveVoltage(leftVoltage, rightVoltage);
     prevError = error;
     pros::delay(20);
   }
-  resetDriveMotors();
-}
-
-
-
-void setDriveGains(double kP, double kI, double kD)
-{
-  drive.kP = kP;
-  drive.kI = kI;
-  drive.kD = kD;
+  resetDriveEncoders();
 }
