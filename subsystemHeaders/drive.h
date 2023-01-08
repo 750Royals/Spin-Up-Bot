db7b8f86840f0479d43666172001b3c426807ffa
mayankd101 <69910481+mayankd101@users.noreply.github.com>
2023-01-08 11:43:52 -0500

    Update drive.h

--- subsystemHeaders/drive.h
+++ subsystemHeaders/drive.h
@@ -1,10 +1,4 @@
 #include "main.h"
 
 int deadzone(int value, int deadzone);
-void setDriveVoltage(double voltage);
-void resetDriveMotors();
-double convert(double inches);
-double curveControls(int value, double min, int exponent);
 void setDriverControls();
-void indexerControl();
-void rollerControl();
