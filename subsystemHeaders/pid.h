f4d1fdabd92b7e63ec4c469da24c66f530711f06
mayankd101 <69910481+mayankd101@users.noreply.github.com>
2023-01-08 11:44:12 -0500

    Update pid.h

--- subsystemHeaders/pid.h
+++ subsystemHeaders/pid.h
@@ -3,12 +3,13 @@
 extern double kP;
 extern double kI;
 extern double kD;
-extern int target;
-extern int current;
-extern int error;
-extern int prevError;
-extern int derivative;
-extern int integral;
-extern void moveDistance(double distance);
+extern double tkP;
+extern double tkI;
+extern double tkD;
+extern void moveDistance(int value);
+extern void turn(int value);
 extern void setDriveVoltage(int leftVoltage, int rightVoltage);
 extern int convert(double inches);
+extern void turn();
+extern void resetDriveEncoders();
+
