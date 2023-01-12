083de2df887162375fe5f2feddc76e8f975840ae
mayankd101 <69910481+mayankd101@users.noreply.github.com>
2023-01-11 20:42:39 -0500

    Update autonomous.cpp

--- subsytems/autonomous.cpp
+++ subsytems/autonomous.cpp
@@ -1,11 +1,20 @@
 #include "main.h"
+#include "subsystemHeaders/autonomous.h"
+#include "subsystemHeaders/driver.h"
+#include "subsystemHeaders/globals.h"
+#include "subsystemHeaders/autonmethods.h"
 
 
 void left_auton_pid()
 {
   resetDriveEncoders();
-  pros::delay(50);
-  turn(2400); //180 degree turn
-  pros::delay(50);
-  moveDistance(1337); //2 feet distance
+  /*moveIntake(1,0.25);
+  moveDistance(20);
+  turn(90);
+  moveDistance(-23);
+  moveIntake(1,0.25);*/
+  //moveDistance(21);
+  turn(90);
+  //turn(-45);
+  moveDistance(21);
 }
