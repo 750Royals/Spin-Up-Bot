eb586de6b38605acb964178da9ae6ff3b32e7e35
mayankd101 <69910481+mayankd101@users.noreply.github.com>
2023-01-14 02:15:30 -0500

    Update globals.cpp

--- subsytems/globals.cpp
+++ subsytems/globals.cpp
@@ -1,14 +1,18 @@
 #include "main.h"
+#include "subsystemHeaders/autonomous.h"
+#include "subsystemHeaders/driver.h"
+#include "subsystemHeaders/globals.h"
+#include "subsystemHeaders/autonmethods.h"
 
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 pros::Motor backRight(6, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor frontRight(20, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor frontRight(1, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor backRightUp(9, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor backLeft(5, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor frontLeft(11, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor backLeftUp(8, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor intake(7, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor flywheel(10, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor intake(7, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor flywheel(10, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
 pros::ADIDigitalOut piston ('G', false);
 pros::Vision visionSensor(12);
